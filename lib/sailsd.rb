b41709888f9c7a25c89eca58b913b7d042343a8d
Louis Taylor <louis@kragniz.eu>
2017-04-20 00:36:32 +0100

    Update variables

--- lib/sailsd.rb
+++ lib/sailsd.rb
@@ -2,6 +2,13 @@ require 'json'
 require 'socket'
 
 class Sailsd
+  attr_reader :latitude
+  attr_reader :longitude
+  attr_reader :heading
+  attr_reader :rudder_angle
+  attr_reader :sail_angle
+  attr_reader :speed
+
   def initialize()
     @socket = nil
   end
@@ -16,8 +23,30 @@ class Sailsd
   end
 
   def send_hash(hash)
-	  bytes = JSON.generate(hash)
+    bytes = JSON.generate(hash)
     ret = send_bytes(bytes)
     return JSON.parse(ret)
+  end
+
+  def update()
+    connect()
+
+    r = send_hash({
+      :request => [
+        'latitude',
+        'longitude',
+        'heading',
+        'rudder-angle',
+        'sail-angle',
+        'speed'
+      ]
+    })
+
+    @latitude = r['latitude']
+    @longitude = r['longitude']
+    @heading = r['heading']
+    @rudder_angle = r['rudder-angle']
+    @sail_angle = r['sail-angle']
+    @speed = r['speed']
   end
 end
